6267943f646524adf42a9e554b32e67b2f644336
joypaul1 <jyla5102555@gmail.com>
2024-08-21 17:41:02 +0600

    line managera arppoval

---
+++ lm_leave_approve_denied.php
@@ -0,0 +1,96 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    $checkValidTokenData = require_once("checkValidTokenData.php");
+    if ($checkValidTokenData['status']) {
+        if ($checkValidTokenData['data']->data->RML_ID) {
+
+            //** ORACLE DATA CONNECTION***//
+            include_once('../rml_hr_api/inc/connoracle.php');
+            if ($isDatabaseConnected !== 1) {
+                $jsonData = ["status" => false, "message" => "Database Connection Failed."];
+                echo json_encode($jsonData);
+                die();
+            }
+            //** ORACLE DATA CONNECTION***//
+
+            require_once('InputValidator.php');  // Include InputValidator class
+            $requiredFields = ['DATAID', 'REMAKRS', 'ACCEPTED_STATUS'];  // Define required fields
+
+            // Initialize input validator with POST data **//
+            $validator = new InputValidator($_POST);
+            if (!$validator->validateRequired($requiredFields)) {
+                // Set the HTTP status code to 400 Bad Request
+                http_response_code(400);
+                $jsonData = ["status" => false, "message" => "Missing Required Parameters."];
+                echo json_encode($jsonData);
+                die();
+            }
+            // **Initialize input validator with POST Data**//
+
+            $validator->sanitizeInputs();   // Sanitize Inputs
+            $DATAID = $validator->get('DATAID');   // Retrieve sanitized inputs
+            $REMAKRS = $validator->get('REMAKRS');   // Retrieve sanitized inputs
+            $ACCEPTED_STATUS = $validator->get('ACCEPTED_STATUS');   // Retrieve sanitized inputs
+            // $RML_ID = $checkValidTokenData['data']->data->RML_ID;
+            // $LINE_MANAGER_RML_ID = $checkValidTokenData['data']->data->LINE_MANAGER_RML_ID;
+            // $ENTRY_BY = $RML_ID;
+
+            //*** Start Query & Return Data Response ***//
+            try {
+                $SQL = "UPDATE RML_HR_EMP_LEAVE SET
+                        IS_APPROVED='$ACCEPTED_STATUS',
+                        LINE_MNGR_APVL_STS='$ACCEPTED_STATUS',
+                        LINE_MNGR_APVL_RKMS='$REMAKRS',
+                        LINE_MNGR_APVL_DATE=SYSDATE
+                        WHERE ID='$DATAID'";
+                $strSQL = @oci_parse($objConnect, $SQL);
+
+                if (@oci_execute($strSQL)) {
+                    $attnSQL = @oci_parse($objConnect, "DECLARE V_START_DATE VARCHAR2(100);
+                    V_END_DATE VARCHAR2(100); V_RML_ID VARCHAR2(100);
+                    BEGIN SELECT TO_CHAR(START_DATE,'dd/mm/yyyy'),TO_CHAR(END_DATE,'dd/mm/yyyy'), RML_ID INTO V_START_DATE,V_END_DATE,V_RML_ID FROM RML_HR_EMP_LEAVE  WHERE ID='$DATAID';
+                    RML_HR_ATTN_PROC(V_RML_ID,TO_DATE(V_START_DATE,'dd/mm/yyyy'),TO_DATE(V_END_DATE,'dd/mm/yyyy'));end;");
+                    @oci_execute($attnSQL);
+
+                    http_response_code(200);
+                    $jsonData = array(
+                        "status" => true,
+                        "message" => "Leave Approval/Denied Successfully Completed.",
+                        "push_message" => "Dear, your leave entry is approved by your line manager.",
+                        'push_id' => '',
+                    );
+                    echo json_encode($jsonData);
+                } else {
+                    http_response_code(403);
+                    @$lastError = error_get_last();
+                    @$error = $lastError ? "" . $lastError["message"] . "" : "";
+                    @$str_arr_error = preg_split("/\,/", $error);
+                    $jsonData = ["status" => false, "message" => @$str_arr_error];
+                    echo json_encode($jsonData);
+                }
+            } catch (Exception $e) {
+                http_response_code(500);
+                $jsonData = ["status" => false, "message" => $e->getMessage()];
+                echo json_encode($jsonData);
+            } finally {
+                oci_close($objConnect);
+            }
+            //*** End Query & Return Data Response ***//
+        } else {
+            // Set the HTTP status code to 400 Bad Request
+            http_response_code(400);
+            $jsonData = ["status" => false, "message" => "Missing Token Required Parameters."];
+            echo json_encode($jsonData);
+        }
+    }
+} else {
+    http_response_code(405);
+    $jsonData = ["status" => false, "message" => "Request method not accepted"];
+    echo json_encode($jsonData);
+}
+die();
